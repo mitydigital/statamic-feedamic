2f1d341a2ddab2db66af10ec5abd6ee58021bde1
Marty Friedel <1491079+martyf@users.noreply.github.com>
2023-03-29 16:42:52 +1100

    Support Statamic 4

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use MityDigital\Feedamic\Http\Controllers\FeedamicController;
 use MityDigital\Feedamic\Feedamic;
 
 // if we are on or above 2.2.0, and we have a 'feeds' config param
@@ -9,7 +10,7 @@ if (Feedamic::version() >= '2.2.0' && config('feedamic.feeds', false)) {
         if (isset($feed['routes']) && is_array($feed['routes'])) {
             collect($feed['routes'])->each(function ($route, $type) use ($key) {
                 // Create a route for the given feed and type
-                Route::get($route, 'FeedamicController@'.$type)
+                Route::get($route, [FeedamicController::class, $type])
                     ->setDefaults(['feed' => $key])
                     ->name('feedamic.'.$key.'.'.$type);
             });
@@ -18,6 +19,6 @@ if (Feedamic::version() >= '2.2.0' && config('feedamic.feeds', false)) {
 } else {
     // v2.1 and below
     collect(config('feedamic.routes'))->each(function ($route, $type) {
-        Route::get($route, 'FeedamicController@'.$type);
+        Route::get($route, [FeedamicController::class, $type]);
     });
 }
